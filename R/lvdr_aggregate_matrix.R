b5e4c4409eda21938717ab699427ff46040c92e2
Jason Timm <jatimm@salud.unm.edu>
2019-08-21 12:55:21 -0600

    re

--- R/lvdr_aggregate_matrix.R
+++ R/lvdr_aggregate_matrix.R
@@ -7,28 +7,28 @@
 #' @param type A part-of-speach (of universal variety) or "PRE"|"SUF"
 #' @return A data.frame
 #' @import Matrix.utils
-#' 
-#' 
+#'
+#'
 #' @export
 #' @rdname lvdr_aggregate_matrix
 
 
-lvdr_aggregate_matrix <- function(tfm, 
-                                  group, 
+lvdr_aggregate_matrix <- function(tfm,
+                                  group,
                                   fun = 'sum') {
-  
+
   ## Alpha order tfm terms/features
-  tfm1 <-  tfm[, order(colnames(tfm))] 
+  tfm1 <-  tfm[, order(colnames(tfm))]
   tfm1 <- tfm1[order(rownames(tfm1)), ]
-  
+
   ##For columns
   tfm1  <- Matrix.utils::aggregate.Matrix(x = tfm1, ## We can sort here, presumably.
-                                          groupings = sort(group), 
+                                          groupings = sort(group),
                                           fun = fun)
-  
-  Matrix.utils::aggregate.Matrix(x = t(tfm1), 
-                                 groupings = descriptors$descriptor_name, 
+
+  Matrix.utils::aggregate.Matrix(x = t(tfm1),
+                                 groupings = sort(group),
                                  fun = fun)
-  
-  ## Assumes x~y == y~x, which we have always been assuming.  
+
+  ## Assumes x~y == y~x, which we have always been assuming.
 }
