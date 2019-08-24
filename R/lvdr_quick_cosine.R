d950c66506af6d66059edca27472aeff87b392fa
Jason Timm <jaetimm@gmail.com>
2019-08-24 12:54:08 -0600

    re

--- R/lvdr_quick_cosine.R
+++ R/lvdr_quick_cosine.R
@@ -17,24 +17,24 @@
 lvdr_quick_cosine <- function (tfm,
                                target,
                                n = 10,
-                               vocab = NULL) {  # Add vocab parameter.
-  
-  if (vocab == NULL) {} else {
+                               vocab = NULL) {
+
+  if (is.na(vocab)) {} else {
     tfm <- tfm[rownames(tfm) %in% unique(vocab),]
   }
-  
-  cos_sim <- text2vec::sim2(x = tfm, 
-                            y = tfm[target, , drop = FALSE], 
-                            method = "cosine", 
+
+  cos_sim <- text2vec::sim2(x = tfm,
+                            y = tfm[target, , drop = FALSE],
+                            method = "cosine",
                             norm = "l2")
 
   x1 <- head(sort(cos_sim[,1], decreasing = TRUE), n+1)
-  
+
   y1 <- data.frame(term1 = target,
                    term2 = names(x1),
                    value = round(x1, 3),
                    stringsAsFactors = FALSE,
                    row.names = NULL)
-  
+
   subset(y1, term1 != term2)
 }
